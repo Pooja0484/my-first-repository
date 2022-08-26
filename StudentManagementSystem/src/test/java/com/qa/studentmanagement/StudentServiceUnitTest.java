73703e22477cf8cb72e1e8ad13a9503287304f71
Pooja0484 <108137868+Pooja0484@users.noreply.github.com>
2022-08-26 15:25:35 +0100

    Update StudentServiceUnitTest.java

--- StudentManagementSystem/src/test/java/com/qa/studentmanagement/StudentServiceUnitTest.java
+++ StudentManagementSystem/src/test/java/com/qa/studentmanagement/StudentServiceUnitTest.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import com.qa.studentmanagement.DTO.StudentDTO;
+
 import com.qa.studentmanagement.entity.Student;
 import com.qa.studentmanagement.repo.StudentRepo;
 import com.qa.studentmanagement.service.StudentService;
