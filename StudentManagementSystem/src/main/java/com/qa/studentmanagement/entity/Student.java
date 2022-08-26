f06011f7aedb20e461307bbd454ea0f29823aa68
Pooja0484 <108137868+Pooja0484@users.noreply.github.com>
2022-08-26 15:11:29 +0100

    Update Student.java

--- StudentManagementSystem/src/main/java/com/qa/studentmanagement/entity/Student.java
+++ StudentManagementSystem/src/main/java/com/qa/studentmanagement/entity/Student.java
@@ -1,10 +1,15 @@
 package com.qa.studentmanagement.entity;
-
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -33,5 +38,9 @@ public class Student {
 	    
 	    @Column
 	    private Long contact_no;
-	    
+	   
+	    @OneToMany(mappedBy = "student", fetch = FetchType.EAGER)
+	    @OnDelete(action = OnDeleteAction.CASCADE)
+	    private List<Course> course = new ArrayList<>();
+    
 }
