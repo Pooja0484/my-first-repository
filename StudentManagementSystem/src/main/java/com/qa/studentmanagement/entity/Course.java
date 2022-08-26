aab25ba251a09f8643364b919ec13d12919a5c83
Pooja0484 <108137868+Pooja0484@users.noreply.github.com>
2022-08-26 15:14:00 +0100

    Update Course.java

--- StudentManagementSystem/src/main/java/com/qa/studentmanagement/entity/Course.java
+++ StudentManagementSystem/src/main/java/com/qa/studentmanagement/entity/Course.java
@@ -5,7 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+import javax.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -32,6 +32,8 @@ import lombok.Setter;
 		    
 		    @Column
 		    private float fees;
-		    
+		   
+		    @ManyToOne
+		    private Student student;  
 	}
 
