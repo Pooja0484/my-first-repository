dd0f25d585ed2d82b3c68daa71a1f551c0abcf62
Pooja0484 <108137868+Pooja0484@users.noreply.github.com>
2022-08-26 15:47:12 +0100

    Update StudentRepo.java

--- StudentManagementSystem/src/main/java/com/qa/studentmanagement/repo/StudentRepo.java
+++ StudentManagementSystem/src/main/java/com/qa/studentmanagement/repo/StudentRepo.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.qa.studentmanagement.DTO.StudentDTO;
+
 import com.qa.studentmanagement.entity.Student;
 
 @Repository
